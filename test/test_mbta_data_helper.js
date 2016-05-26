d288931983e4f36a8dd37031f97bd2acae3b81ad
Dan Lourenco <daniel.lourenco@mheducation.com>
2016-05-26 17:42:44 -0400

    wip

--- test/test_mbta_data_helper.js
+++ test/test_mbta_data_helper.js
@@ -1,3 +1,8 @@
 'use strict';
 var chai = require('chai');
 var expect = chai.expect;
+var MBTADataHelper = require('../mbta_data_helper');
+
+describe('MBTADataHelper', function() {
+  var subject = new MBTADataHelper();
+});
