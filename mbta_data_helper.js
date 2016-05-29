06d953a0b9b5d0aa8e72b28a4d69a89825ee9f6c
Dan Lourenco <daniel.lourenco@mheducation.com>
2016-05-29 15:38:16 -0400

    add request all stops, predictions by stop methods

--- mbta_data_helper.js
+++ mbta_data_helper.js
@@ -4,9 +4,10 @@ var _ = require('lodash');
 
 var ROUTES_ENDPOINT = 'http://realtime.mbta.com/developer/api/v2/routes?api_key=wX9NwuHnZU2ToO7GmGR9uw&format=json';
 
-var STOPS_BY_ROUTE_ENDPOINT = 
+var STOPS_BY_ROUTE_ENDPOINT = 'http://realtime.mbta.com/developer/api/v2/stopsbyroute?api_key=wX9NwuHnZU2ToO7GmGR9uw&route=Red&format=json';
 
 var routesFile = require('./routes.json');
+var stopsFile  = require('./stops.json');
 var routes = routesFile.mode;
 
 function MBTADataHelper() {}
@@ -15,7 +16,7 @@ MBTADataHelper.prototype.requestAllRoutes = function() {
   return this.getAllRoutes().then(function(response) {
     console.log('success - received all routes');
     return response.body;
-  })
+  });
 };
 
 MBTADataHelper.prototype.getAllRoutes = function() {
@@ -28,6 +29,7 @@ MBTADataHelper.prototype.getAllRoutes = function() {
   return rp(options);
 };
 
+// refactor to deep search through a generic collection
 MBTADataHelper.prototype.findRouteIdByName = function(obj, name) {
   var arr = [];
   for (var i = 0; i < routes.length; i++) {
@@ -39,5 +41,40 @@ MBTADataHelper.prototype.findRouteIdByName = function(obj, name) {
   var match = _.find(flattened, {route_name: name});
   return match.route_id;
 };
+
+MBTADataHelper.prototype.requestAllStops = function() {
+  return this.getAllStops().then(function(response) {
+    console.log('success - received all stops');
+    return response.body;
+  });
+}
+
+MBTADataHelper.prototype.getAllStops = function() {
+  var options = {
+    method: 'GET',
+    uri: STOPS_BY_ROUTE_ENDPOINT,
+    resolveWithFullResponse: true,
+    json: true
+  };
+  return rp(options);
+}
+
+MBTADataHelper.prototype.requestPredictionsByStop = function(stopId) {
+  return this.getPredictionsByStop(stopId).then(function(response) {
+    console.log('success - received predictions by stop');
+    return response.body;
+  });
+}
+
+MBTADataHelper.prototype.getPredictionsByStop = function(stopId) {
+  let PREDICTIONS_BY_STOP_ENDPOINT = `http://realtime.mbta.com/developer/api/v2/predictionsbystop?api_key=wX9NwuHnZU2ToO7GmGR9uw&stop=${stopId}&format=json`;
+  var options = {
+    method: 'GET',
+    uri: PREDICTIONS_BY_STOP_ENDPOINT,
+    resolveWithFullResponse: true,
+    json: true
+  };
+  return rp(options);
+}
 
 module.exports = MBTADataHelper;
--- test/test_mbta_data_helper.js
+++ test/test_mbta_data_helper.js
@@ -5,6 +5,7 @@ chai.use(chaiAsPromised);
 var expect = chai.expect;
 var MBTADataHelper = require('../mbta_data_helper');
 var routes = require('../routes');
+var stops = require('../stops');
 
 chai.config.includeStack = true;
 
@@ -29,6 +30,32 @@ describe('MBTADataHelper', function() {
       it('returns the appropriate route_id', function() {
         var routeId = subject.findRouteIdByName(routes, 'Green Line E');
         return expect(routeId).to.eq('Green-E');
+      });
+    });
+  });
+
+  describe('#getAllStops', function() {
+    var subject = new MBTADataHelper();
+    context('with a valid request', function() {
+      it('returns a list of all stops (by route)', function() {
+        var allStops = subject.requestAllStops().then(function(response) {
+          return response;
+        });
+        return expect(allStops).to.eventually.exist;
+        // return expect(allStops).to.eventually.equal(stops);
+      });
+    });
+  });
+
+  describe('#getPredictionsByStop', function() {
+    var subject = new MBTADataHelper();
+    context('#with a valid stop ID', function() {
+      it('returns a list of predictions for the next hour', function() {
+        var stopId = 70224;
+        var stopName = subject.requestPredictionsByStop(stopId).then(function(response) {
+          return response.stop_name;
+        });
+        return expect(stopName).to.eventually.eq('Summit Avenue - Inbound');
       });
     });
   });
